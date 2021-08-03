44521ef77c6b32ee803e975cf6e97af9ba83ad3c
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2021-08-03 09:52:49 +0200

    Refactored signup validation method and Register method

--- src/main/model/user.java
+++ src/main/model/user.java
@@ -1,17 +1,16 @@
 package main.model;
 
-import DB.DBConnection;
-import DB.DBOperations;
 
+import DB.DBOperations;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javafx.util.Pair;
-import java.sql.Statement;
 
 public class user {
 
 
     private static final String [] types  = new String[]{"Seeker", "Poster", "Admin"};
+
 
     public static Pair<Boolean,String> isValid(String email, String password) throws SQLException {
 
@@ -34,21 +33,27 @@ public class user {
 
     public static boolean isValid (String fname, String lname, String email, String password) throws SQLException {
 
-        String query = "SELECT Email FROM Admin";
-        ResultSet rs = DBOperations.Read(query);
+        String query = "SELECT Email FROM ";
+        ResultSet rs ;
 
-        while(rs.next()){
-            if (email.equals(rs.getString("Email"))){
-                return false;
+        for (int i = 0 ; i < 3 ; i++) {
+
+            rs = DBOperations.Read(query + types[i]);
+
+            while (rs.next()) {
+                if (email.equals(rs.getString("Email"))) {
+                    return false;
+                }
             }
+
         }
         return fname.matches("[a-zA-z]+") && lname.matches("[a-zA-z]+") && email.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
     }
 
     public static void Register (String fname, String lname, String email, String password) throws SQLException {
-        String values = "( '" + email + "', '" + lname + "', '" + fname + "', '" + password + "', '" + "Seeker'" + ')';
-       // System.out.println(values);
-        String query = "insert into user (Email, Lname, Fname, Password, user_type) values " + values;
+        String values = "( '" + fname + "', '" + lname + "', '" + email + "', '" + password + " ')" ;
+        //System.out.println(values);
+        String query = "insert into seeker (Fname, Lname, Email, Password) values " + values;
         DBOperations.Create(query);
 
     }
