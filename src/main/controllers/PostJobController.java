e4baf787b54c62e5d51a73d39f372d7d518da666
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2021-08-12 11:35:58 +0200

    Created a validation method to validate the attributes of new jobs, and insert valid jobs to the database

--- src/main/controllers/PostJobController.java
+++ src/main/controllers/PostJobController.java
@@ -3,6 +3,10 @@ package main.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import main.model.Job;
+import main.model.JobPoster;
+
+import java.sql.SQLException;
 
 public class PostJobController {
 
@@ -20,11 +24,25 @@ public class PostJobController {
 
 
     @FXML
-    public void postButtonAction(){
+    public void postButtonAction() throws SQLException {
 
         String jobTitle = jobTitleTextField.getText(),
                jobSalary = salaryTextField.getText(),
                jobDescription = descriptionTextField.getText();
+
+        if (Job.isValid(jobTitle, jobDescription, jobSalary)){
+            int salary = Integer.parseInt(jobSalary);
+            Job job = new Job (jobTitle, jobDescription, salary);
+            JobPoster.postNewJob(job);
+            postStateMessage.setText("New Job has been posted successfully");
+            postStateMessage.visibleProperty().set(true);
+            jobTitleTextField.clear();
+            salaryTextField.clear();
+            descriptionTextField.clear();
+        }else {
+            postStateMessage.setText("Incorrect Data");
+            postStateMessage.visibleProperty().set(true);
+        }
 
 
 
--- src/main/model/Job.java
+++ src/main/model/Job.java
@@ -2,5 +2,20 @@ package main.model;
 
 public class Job {
 
+    String Title, Description;
+    int Salary;
+
+    public Job(String Title, String Description, int Salary){
+        this.Title = Title;
+        this.Description = Description;
+        this.Salary = Salary;
+    }
+
+
+
+    public static boolean isValid (String Title, String Description, String Salary){
+
+        return Title.matches("^[a-zA-Z ]*$") && Salary.matches("[0-9]+") && Description.matches("^[a-zA-Z ]*$");
+    }
 
 }
