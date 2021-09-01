5726eafb3dd80ae40861e53cf1b4c74e81353789
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2021-09-01 11:55:50 +0200

    Created fetch method to get all the jobs data from the DB

--- src/main/model/Job.java
+++ src/main/model/Job.java
@@ -1,8 +1,13 @@
 package main.model;
 
+import DB.DBOperations;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Job {
 
-    String Title, Description;
+    String Title, Description, companyName;
     int Salary;
 
     public Job(String Title, String Description, int Salary){
@@ -16,6 +21,14 @@ public class Job {
     public static boolean isValid (String Title, String Description, String Salary){
 
         return Title.matches("^[a-zA-Z ]*$") && Salary.matches("[0-9]+") && Description.matches("^[a-zA-Z ]*$");
+    }
+
+    public static void fetch () throws SQLException {
+        String query = "SELECT job.Title, job.Salary, job.Description, company.Name " +
+                       "FROM job " +
+                       "INNER JOIN company ON job.C_ID = company.ID";
+
+        ResultSet rs = DBOperations.Read(query);
     }
 
 }
